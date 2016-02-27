a4daa72428489e2cdceaac0212b4c09750f4357f
Gaetitan <gaetitan@gmail.com>
2016-02-27 14:52:25 +0100

    Class Connect.java finished
    Signed-off-by: Gaetitan <gaetitan@gmail.com>

--- Goldfish/src/com/polytech/goldfish/application/Login.java
+++ Goldfish/src/com/polytech/goldfish/application/Login.java
@@ -1,5 +1,12 @@
 package com.polytech.goldfish.application;
 
+
+/**
+ * This class allows a user to login the application
+ * 
+ * @author Gaëtan FRANÇOIS
+ *
+ */
 public class Login {
 
 }
--- Goldfish/src/com/polytech/goldfish/util/Connect.java
+++ Goldfish/src/com/polytech/goldfish/util/Connect.java
@@ -1,60 +1,56 @@
 package com.polytech.goldfish.util;
+
 import java.sql.Connection;
 import java.sql.DriverManager;
-import java.sql.ResultSet;
-import java.sql.ResultSetMetaData;
-//CTRL + SHIFT + O pour générer les imports
+import java.sql.SQLException;
+
+/**
+ * This class connects the application to a database
+ * @author Gaëtan FRANÇOIS
+ *
+ */
 public class Connect {
-  public static void main(String[] args) {      
-    try {
-      Class.forName("org.postgresql.Driver");
-      System.out.println("Driver O.K.");
-      String url = "jdbc:postgresql://ec2-107-20-153-141.compute-1.amazonaws.com:5432/deo26s6g50b66u?sslmode=require";
-      String user = "neuakxuifmnrfv";
-      String passwd = "tlK7w4GuufFaIzOyvaoTmu8KkK";
-      
-      System.out.println("Connexion sur: ");
-      System.out.println("**USER = " +user);
-      System.out.println("**PassWord = " +passwd);
-      System.out.println("**URL = " +url);
-      Connection conn = DriverManager.getConnection(url, user, passwd);
-      System.out.println("\nConnexion effective !");
-      
-    //STEP 4: Execute a query
-      java.sql.Statement stmt = null;
-      System.out.println("Creating statement...");
-      stmt = conn.createStatement();
-      String sql = "SELECT * FROM ticks";
-      ResultSet rs = stmt.executeQuery(sql);
-      ResultSetMetaData resultMeta = rs.getMetaData();
-      
-    //STEP 5: Extract data from result set
-      System.out.println("\n************************************************************************************************************************************************************************************");
-      for(int i = 1; i <= resultMeta.getColumnCount(); i++)
-          System.out.print("\t" + resultMeta.getColumnName(i).toUpperCase() + "\t *");
-      System.out.println("\n************************************************************************************************************************************************************************************");
-      
-      
-      while(rs.next()){
-         //Retrieve by column name
-         /*String id_user  = rs.getString("iduser");
-         String email_user  = rs.getString("email");
-         String passWord_user  = rs.getString("password");
-         //Display values
-         System.out.print("ID: " + id_user +"\n");
-         System.out.print("EMAIL: " + email_user +"\n");
-         System.out.print("PASSWORD: " + passWord_user +"\n");*/
-    	  
-    	  for(int i = 1; i <= resultMeta.getColumnCount(); i++)
-              System.out.print("\t" + rs.getObject(i).toString() + "\t |");
-                
-            System.out.println("\n------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------");
-      }
-      rs.close();
-      
-    } catch (Exception e) {
-      e.printStackTrace();
-    }  
-    System.out.println("\n... Statement finish");
-  }
+	private static Connect instance = null;
+	private String driverName = "org.postgresql.Driver";
+	private String url;
+	private String user;
+	private String password;
+	
+	private Connect() {
+		this.url = "jdbc:postgresql://ec2-107-20-153-141.compute-1.amazonaws.com:5432/deo26s6g50b66u?sslmode=require";
+		this.user = "neuakxuifmnrfv";
+		this.password = "tlK7w4GuufFaIzOyvaoTmu8KkK";
+		
+		try{
+			Class.forName(this.driverName);
+		}
+		catch(ClassNotFoundException e){
+			e.printStackTrace();
+		}
+	}
+	
+	public static synchronized Connect getInstance(){
+		if(instance == null){
+			instance = new Connect();
+		}
+		return instance;
+	}
+	
+	/**
+	 * This method allows the connection to the database
+	 * 
+	 * @return the connection to the database
+	 */
+	public Connection getConnection(){
+		Connection connection = null;
+		try{
+			connection = DriverManager.getConnection(this.url, this.user, this.password);
+			connection.setAutoCommit(false);
+		}
+		catch(SQLException e){
+			e.printStackTrace();
+		}
+		return connection;
+	}
+	
 }
