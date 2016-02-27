ec4af6f3900fb8d91abacf10eca29605fba6e200
Gaetitan <gaetitan@gmail.com>
2016-02-27 15:38:55 +0100

    FrameLogin added + Login changed
    Signed-off-by: Gaetitan <gaetitan@gmail.com>

--- Goldfish/src/com/polytech/goldfish/application/Login.java
+++ Goldfish/src/com/polytech/goldfish/application/Login.java
@@ -1,5 +1,9 @@
 package com.polytech.goldfish.application;
 
+import java.sql.Connection;
+
+import com.polytech.goldfish.util.Connect;
+
 
 /**
  * This class allows a user to login the application
@@ -8,5 +12,9 @@ package com.polytech.goldfish.application;
  *
  */
 public class Login {
-
+	private Connection connect = Connect.getInstance().getConnection();
+	
+	public void authenticate(String email, String password){
+		
+	}
 }
