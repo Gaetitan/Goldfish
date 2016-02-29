041ddddc25fbfe38aadf69d952549795830816a4
RedaMaachiGit <maachi.reda@gmail.com>
2016-02-29 16:23:29 +0100

    Changement mineur dans le main

--- Goldfish/src/com/polytech/goldfish/main/Main.java
+++ Goldfish/src/com/polytech/goldfish/main/Main.java
@@ -9,7 +9,7 @@ public class Main {
 	public static void main(String[] args) {
 		MainFrame myFrame = new MainFrame();
 		myFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		myFrame.setTitle("Goldfish");
+		myFrame.setTitle("Goldfish project");
 		myFrame.setSize(600, 500);
 		myFrame.setVisible(true);
 	}
